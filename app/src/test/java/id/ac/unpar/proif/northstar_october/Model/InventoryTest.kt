fbd28748e83359302eb1a7047247c8d8385fe946
jason-61 <6181801061@student.unpar.ac.id>
2022-03-24 10:36:56 +0700

    create InventoryTest.kt

---
+++ app/src/test/java/id/ac/unpar/proif/northstar_october/Model/InventoryTest.kt
@@ -0,0 +1,19 @@
+package id.ac.unpar.proif.northstar_october.Model
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import java.util.ArrayList
+
+class InventoryTest{
+
+
+    @Test
+    fun inventoryTest(){
+
+        assertTrue(true)
+
+    }
+
+
+}
