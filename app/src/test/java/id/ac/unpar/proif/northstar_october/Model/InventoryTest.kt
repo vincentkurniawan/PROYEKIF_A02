b71f23e6150afcb858a3882bcf67f6adbedffdb3
jason-61 <6181801061@student.unpar.ac.id>
2022-03-24 13:40:55 +0700

    update Inventorytest

--- app/src/test/java/id/ac/unpar/proif/northstar_october/Model/InventoryTest.kt
+++ app/src/test/java/id/ac/unpar/proif/northstar_october/Model/InventoryTest.kt
@@ -3,10 +3,21 @@ package id.ac.unpar.proif.northstar_october.Model
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
 import java.util.ArrayList
 
+@RunWith(MockitoJUnitRunner::class)
 class InventoryTest{
 
+    @Mock
+    lateinit var inv: Inventory
+
+    @Before
+    fun setup(){
+        this.inv = Inventory()
+    }
 
     @Test
     fun inventoryTest(){
